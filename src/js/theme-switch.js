1e0c2fa8b711be21b8a26573de9752949655eab9
FoxCrush <63466872+FoxCrush@users.noreply.github.com>
2021-03-12 13:53:26 +0200

    js restructure

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -4,20 +4,59 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-if (bodyEl) {
+
+function swithingToLightTheme() {
+    localStorage.setItem('theme', Theme.LIGHT);
+    bodyEl.classList.replace(Theme.DARK, Theme.LIGHT);
+}
+function swithingToDarkTheme() {
+    localStorage.setItem('theme', Theme.DARK);
+    bodyEl.classList.replace(Theme.LIGHT, Theme.DARK);
+}
+function setStarterLocalStorage() {
     bodyEl.classList.add(Theme.LIGHT);
     localStorage.setItem('theme', Theme.LIGHT);
-    themeSwitchEl.checked = false;
 }
+function setStarterBodyTheme() {
+    bodyEl.classList.add(localStorage.getItem('theme'));
+    themeSwitchEl.checked = localStorage.getItem('theme') === Theme.DARK;
+}
+function restoringLocalStorageToDarkTheme() {
+    bodyEl.classList.remove(Theme.LIGHT);
+    bodyEl.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+}
+function restoringLocalStorageToLightTheme() {
+    bodyEl.classList.remove(Theme.DARK);
+    bodyEl.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+}
+
+if (!localStorage.getItem('theme')) {
+    setStarterLocalStorage();
+}
+if (bodyEl.classList.length === 0) {
+    setStarterBodyTheme();
+}
+
 function switchElToggled() {
     if (localStorage.getItem('theme') === Theme.LIGHT) {
-        localStorage.setItem('theme', Theme.DARK);
-        bodyEl.classList.replace(Theme.LIGHT, Theme.DARK);
-
+        swithingToDarkTheme();
     } else if (localStorage.getItem('theme') === Theme.DARK) {
-        localStorage.setItem('theme', Theme.LIGHT);
-        bodyEl.classList.replace(Theme.DARK, Theme.LIGHT);
-
+        swithingToLightTheme();
+    }
+    else if (!localStorage.getItem('theme')) {
+        if (themeSwitchEl.checked) {
+            restoringLocalStorageToDarkTheme();
+        }
+        else {
+            restoringLocalStorageToLightTheme();
+        }
     }
 };
+
 themeSwitchEl.addEventListener('change', switchElToggled);
+
+if (!bodyEl.classList.value === localStorage.getItem('theme')) {
+    localStorage.removeItem('theme');
+}
